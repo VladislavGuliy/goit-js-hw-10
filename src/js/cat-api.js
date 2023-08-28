781c8768f2047f959ee52e2a3440a56d4800ced2
VladislavGuliy <vladgulij777@gmail.com>
2023-08-29 01:31:31 +0300

    Update cat-api.js

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,16 +1,20 @@
 import axios from 'axios';
 
-const catApi = axios.create({
-  baseURL: 'https://api.thecatapi.com/v1',
-  headers: {
-    'x-api-key':
-      'live_M0pIFaq2Ee9EjbJ7AbfsCesAjbqKbQSMCI9EnF5SMc6gON19e7GiU9ylgu3DpBVU',
-  },
-});
+
+// const catApi = axios.create({
+//   baseURL: 'https://api.thecatapi.com/v1',
+//   headers: {
+//     'x-api-key':
+//       'live_M0pIFaq2Ee9EjbJ7AbfsCesAjbqKbQSMCI9EnF5SMc6gON19e7GiU9ylgu3DpBVU',
+//   },
+// });
+
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
+axios.defaults.headers.common['x-api-key'] = 'live_M0pIFaq2Ee9EjbJ7AbfsCesAjbqKbQSMCI9EnF5SMc6gON19e7GiU9ylgu3DpBVU';
 
 export async function fetchBreeds() {
   try {
-    const resp = await catApi
+    const resp = await axios
       .get('/breeds');
     return resp.data;
   } catch (error) {
@@ -20,7 +24,7 @@ export async function fetchBreeds() {
 
 export async function fetchCatByBreed(breedId) {
   try {
-    const resp = await catApi
+    const resp = await axios
       .get(`/images/search?breed_ids=${breedId}`);
     return resp.data;
   } catch (error) {
